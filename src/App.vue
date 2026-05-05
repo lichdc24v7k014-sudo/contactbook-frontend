fcee6a530d3c80b398c3b48320942a826972bca8
Hoàng Lịch Nguyễn <108135808+lichnguyengithub162003@users.noreply.github.com>
2026-05-05 12:27:10 +0700

    Khởi tạo dự án Vue - Bước 2

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<template>
+  <div class="page">
+    <h1>Hello, Vue.js!</h1>
+  </div>
+</template>
+
+<style>
+.page {
+  max-width: 800px;
+  margin: auto;
+  padding: 40px;
+  text-align: center;
+  font-family: Arial, sans-serif;
+}
+</style>
